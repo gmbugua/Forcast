4a1e0fd02fe7888a5b8de5a37accbd8f5adb5fd8
George Mbugua <gmbugua2020@gmail.com>
2020-08-31 18:37:44 -0700

    forcast page layout test

--- src/components/App.js
+++ src/components/App.js
@@ -5,54 +5,16 @@ import styles from "./App.module.scss";
 function App() {
   return (
     <Wrapper>
-      {/* <p className="text--xxxl">
-          <b>Hero Text</b>{" "}
-        </p> */}
       <div className={styles.container}>
-        <div>
-          <h1>heading1</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam
-            quisquam quaerat nemo fugiat quam, commodi et voluptatum ut, velit
-            suscipit labore? Accusamus, illo culpa? Consequatur temporibus illo
-            laborum repellendus sint saepe ea sequi voluptates veniam veritatis
-            officiis quaerat doloremque doloribus iure, nobis expedita inventore
-            fuga nam corporis quidem vel! Magnam.
-          </p>
-        </div>
-        <div>
-          <h2>heading2</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam
-            quisquam quaerat nemo fugiat quam, commodi et voluptatum ut, velit
-            suscipit labore? Accusamus, illo culpa? Consequatur temporibus illo
-            laborum repellendus sint saepe ea sequi voluptates veniam veritatis
-            officiis quaerat doloremque doloribus iure, nobis expedita inventore
-            fuga nam corporis quidem vel! Magnam.
-          </p>
-        </div>
-        <div>
-          <h3>heading3</h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam
-            quisquam quaerat nemo fugiat quam, commodi et voluptatum ut, velit
-            suscipit labore? Accusamus, illo culpa? Consequatur temporibus illo
-            laborum repellendus sint saepe ea sequi voluptates veniam veritatis
-            officiis quaerat doloremque doloribus iure, nobis expedita inventore
-            fuga nam corporis quidem vel! Magnam.
-          </p>
-        </div>
-        <div>
-          <h4>heading4</h4>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam
-            quisquam quaerat nemo fugiat quam, commodi et voluptatum ut, velit
-            suscipit labore? Accusamus, illo culpa? Consequatur temporibus illo
-            laborum repellendus sint saepe ea sequi voluptates veniam veritatis
-            officiis quaerat doloremque doloribus iure, nobis expedita inventore
-            fuga nam corporis quidem vel! Magnam.
-          </p>
-        </div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
       </div>
     </Wrapper>
   );
