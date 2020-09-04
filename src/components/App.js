c59a450ffdf4e87d9fef9a9a6a349640d665715a
George Mbugua <gmbugua2020@gmail.com>
2020-09-04 11:27:34 -0700

    API call loading screen (#2)
    * css fix
    
    * responsive loader implementation revisions
    
    * responsive loader set up, logo transition on timeout start
    
    * Load logo implemented. Shifting back to forcast page

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,10 @@ import React from "react";
 
 // Weather Forcast Imports => WeatherForcast.js
 import Load from "./Load/Load";
-import DayCard from "./WeatherForcast/DayCard";
-import ForcastCard from "./WeatherForcast/ForcastCard";
-import TemperatureHeader from "./WeatherForcast/TemperatureHeader";
-import ForcastHeader from "./WeatherForcast/ForcastHeader";
+// import DayCard from "./WeatherForcast/DayCard";
+// import ForcastCard from "./WeatherForcast/ForcastCard";
+// import TemperatureHeader from "./WeatherForcast/TemperatureHeader";
+// import ForcastHeader from "./WeatherForcast/ForcastHeader";
 
 export default class App extends React.Component {
   render() {
